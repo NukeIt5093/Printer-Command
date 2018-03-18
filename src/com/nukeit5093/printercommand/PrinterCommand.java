f41a8084d0f71640057915256cb7f8b3ea0e60c3
NukeIt5093 <oinhjufr18@mail.com>
2018-03-18 15:50:49 +0100

    Initial commit.

---
+++ src/com/nukeit5093/printercommand/PrinterCommand.java
@@ -0,0 +1,12 @@
+package com.nukeit5093.printercommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PrinterCommand extends JavaPlugin{
+
+    @Override
+    public void onEnable(){
+        System.out.println("GITHUB IS CANCER");
+    }
+
+}
